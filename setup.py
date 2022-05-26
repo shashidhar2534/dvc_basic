31d0b582b5f93cba7cf2f497678fc90189a2e534
tester <komalashashidhar22@gmail.com>
2022-05-26 10:56:37 +0530

    added setup requir gitignore files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="91849",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shashidhar2534/dvc_basic",
+    author_email="komalashashidhar22@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
